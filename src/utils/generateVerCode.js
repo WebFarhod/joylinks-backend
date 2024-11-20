e367250c05b63bff1d42b61a85f4d822fdcaee92
WebFarhod <https://github.com/WebFarhod>
2024-11-20 15:05:36 +0500

    sms

--- src/utils/generateVerCode.js
+++ src/utils/generateVerCode.js
@@ -1,6 +1,7 @@
 const generateVerificationCode = async () => {
   return {
-    code: Math.floor(100000 + Math.random() * 900000).toString(),
+    // code: Math.floor(100000 + Math.random() * 900000).toString(),
+    code: 123456,
     expires: new Date(Date.now() + 2 * 60 * 1000),
   };
 };
